0304febac90e300f490e81b2e1dc1856d1840811
judegiordano <judegiordano@gmail.com>
2022-07-08 20:16:43 -0400

    Rust Use memchr

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,27 +1,31 @@
-use std::{env, fs::read_to_string, time::Instant};
+use memchr::memmem::{self, Finder};
+use std::{env::{self, Args}, fs::read_to_string, time::{Instant, Duration}};
 
 pub fn get_file_contents(query: &str, filename: &str) {
-    let contents = read_to_string(String::from(filename)).unwrap();
-    let results = search(&query, &contents);
-    let count = results.len();
-    println!("matches: {:#?}", count);
+    let contents: String = read_to_string(String::from(filename)).unwrap();
+    let results: Vec<&str> = search(&query, &contents);
+    println!("matches: {:#?}", results.len());
 }
 
 pub fn search<'a>(query: &'a str, content: &'a str) -> Vec<&'a str> {
-    content
-        .lines()
-        .filter(|line| line.contains(query))
-        .collect()
+    let needle: Finder = memmem::Finder::new(query);
+    let mut results: Vec<&str> = vec![];
+    for line in content.lines() {
+        if let Some(_) = needle.find(line.as_bytes()) {
+            results.push(line)
+        }
+    }
+    results
 }
 
 fn main() {
-    let start = Instant::now();
-    let mut args = env::args();
+    let start: Instant = Instant::now();
+    let mut args: Args = env::args();
     args.next();
-    let query = args.next().unwrap();
-    let file = args.next().unwrap();
+    let query: String = args.next().unwrap();
+    let file: String = args.next().unwrap();
     //
     get_file_contents(&query, &file);
-    let duration = start.elapsed();
+    let duration: Duration = start.elapsed();
     println!("operation complete in {:#?}", duration);
 }
